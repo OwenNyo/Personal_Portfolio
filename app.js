1a91da484076aa91f25e4859992d6bb9343aa4a9
OwenNyo <owennyowy@gmail.com>
2021-12-06 23:34:26 +0800

    Advanced_portfolio
    - Nav
    - Header
    - Footer
    - Modal

---
+++ app.js
@@ -0,0 +1,46 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_2m49u12",
+      "template_jend7ir",
+      event.target,
+      "user_PI90cCavFszPZE0ot00mM"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on owennyowy@gmail.com"
+      );
+    });
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+      document.body.classList += " dark-theme"
+    }
+    else {
+      document.body.classList.remove("dark-theme")
+    }
+  }
+  
