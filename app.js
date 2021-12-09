879ef46870a43e61429d9f3808e0ab6f6e5af1a2
OwenNyo <owennyowy@gmail.com>
2021-12-09 11:31:55 +0800

    1.06
    Addition of SVG background
    Media Queries Added.

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${
+      y * boolInt
+    }px) rotate(${x * boolInt * 10}deg)`;
+  }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -35,12 +50,14 @@ function toggleModal() {
 }
 
 function toggleContrast() {
-    contrastToggle = !contrastToggle;
-    if (contrastToggle) {
-      document.body.classList += " dark-theme"
-    }
-    else {
-      document.body.classList.remove("dark-theme")
-    }
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+   
+  } else {
+    document.body.classList.remove("dark-theme");
+
   }
-  
+}
+
+
